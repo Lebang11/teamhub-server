07d3093210a156fff6a2ffa11f80e879511f8fba
Lebang <lebangnong@gmail.com>
2023-08-26 13:42:59 +0200

    Front end form finished. learning nodejs

---
+++ Scripts/create.js
@@ -0,0 +1,20 @@
+userObject = {
+    name: "Lebang Nong",
+    email: "lebangnong@gmail.com",
+    prefferedName: "Lebang"
+};
+
+users = [];
+
+function addDetails() {
+    const nameBox = document.querySelector('.js-full-name');
+    const name = nameBox.value;
+    console.log("hi");
+};
+
+
+
+document.querySelector('.js-submit-button').addEventListener('click', () => {
+    addDetails();
+});
+
