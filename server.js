84f8dabc0a7993a359c5b1b63b27df9f1ee191ae
L.Nong <lnong023@student.wethinkcode.co.za>
2023-09-19 08:54:09 +0200

    fixing port

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 app.use('/main', mainPage)
 
 
-app.listen(PORT, () => console.log(`Now listening at https://team-hub.onrender.com/`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Now listening at https://team-hub.onrender.com`));
 
 //Testing :
 
