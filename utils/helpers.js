e4708e3982fa636eea32285e4d78a885ef1c37dd
lebang11 <lebangnong@gmail.com>
2023-09-20 22:14:05 +0200

    added async to bcrypt

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,12 @@
 const bcrypt = require('bcryptjs');
 
-const hashPassword = (password) => {
-    const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(password, salt);
+const hashPassword = async function (password) {
+    const salt = await bcrypt.genSaltSync(10);
+    const hashedPassword = await bcrypt.hashSync(password, salt);
 }
 
-function comparePassword(raw, hash) {
-    return bcrypt.compareSync(raw, hash);
+const comparePassword = async function (raw, hash) {
+    return await bcrypt.compareSync(raw, hash);
 }
 
 module.exports = {
