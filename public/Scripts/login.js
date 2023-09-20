ceab5eaabc1c9bb706934cbfbf0e15259597ed46
L.Nong <lnong023@student.wethinkcode.co.za>
2023-09-20 10:27:51 +0200

    using 406 and responding with json

--- public/Scripts/login.js
+++ public/Scripts/login.js
@@ -13,121 +13,126 @@ router.get('/', (req,res)=> {
 router.post('/',async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
-        res.status(401);
-        res.send(`
-    <style>
-        .submit-button {
-            font-family: Roboto;
-            font-size: 20px;
-            background-color: rgb(255, 255, 255);
-            height: 50px;
-            width: 100px;
-            border: none;
-            border-radius: 10px;
-            cursor: pointer;
-            transition: all 0.15s;
-        }
+        res.status(406);
+        res.json({"message": "Enter valid information"})
+    //     res.send(`
+    // <style>
+    //     .submit-button {
+    //         font-family: Roboto;
+    //         font-size: 20px;
+    //         background-color: rgb(255, 255, 255);
+    //         height: 50px;
+    //         width: 100px;
+    //         border: none;
+    //         border-radius: 10px;
+    //         cursor: pointer;
+    //         transition: all 0.15s;
+    //     }
         
-        .submit-button:hover {
-            color: white;
-            background-color: aqua;
-            transform: scale(1.2);
-        }
+    //     .submit-button:hover {
+    //         color: white;
+    //         background-color: aqua;
+    //         transform: scale(1.2);
+    //     }
     
 
-        .back-button {
-            height: 30px;
-            background-color: rgb(66,69,73);
-            color: white;
-        }
+    //     .back-button {
+    //         height: 30px;
+    //         background-color: rgb(66,69,73);
+    //         color: white;
+    //     }
         
-        .back-button:hover {
-            background-color: rgb(191,155,48);
-        }
-    </style>
-    <a href="https://team-hub.netlify.app/login">
-    <button class="submit-button back-button">Back</button>
-    </a>
-    <p>Enter valid details</p>
-    `)};
+    //     .back-button:hover {
+    //         background-color: rgb(191,155,48);
+    //     }
+    // </style>
+    // <a href="https://team-hub.netlify.app/login">
+    // <button class="submit-button back-button">Back</button>
+    // </a>
+    // <p>Enter valid details</p>
+    //`)
+    };
     const userDB = await USER.findOne({email});
     if (!userDB) {
-        res.status(401)
-        return res.send(`
-    <style>
-        .submit-button {
-            font-family: Roboto;
-            font-size: 20px;
-            background-color: rgb(255, 255, 255);
-            height: 50px;
-            width: 100px;
-            border: none;
-            border-radius: 10px;
-            cursor: pointer;
-            transition: all 0.15s;
-        }
+        res.status(406)
+        res.json({"message": "Password is incorrect"})
+    //     return res.send(`
+    // <style>
+    //     .submit-button {
+    //         font-family: Roboto;
+    //         font-size: 20px;
+    //         background-color: rgb(255, 255, 255);
+    //         height: 50px;
+    //         width: 100px;
+    //         border: none;
+    //         border-radius: 10px;
+    //         cursor: pointer;
+    //         transition: all 0.15s;
+    //     }
         
-        .submit-button:hover {
-            color: white;
-            background-color: aqua;
-            transform: scale(1.2);
-        }
+    //     .submit-button:hover {
+    //         color: white;
+    //         background-color: aqua;
+    //         transform: scale(1.2);
+    //     }
     
 
-        .back-button {
-            height: 30px;
-            background-color: rgb(66,69,73);
-            color: white;
-        }
+    //     .back-button {
+    //         height: 30px;
+    //         background-color: rgb(66,69,73);
+    //         color: white;
+    //     }
         
-        .back-button:hover {
-            background-color: rgb(191,155,48);
-        }
-    </style>
-    <a href="https://team-hub.netlify.app/login">
-    <button class="submit-button back-button">Back</button>
-    </a>
-    <p>Password is incorrect</p>
-    `)};
+    //     .back-button:hover {
+    //         background-color: rgb(191,155,48);
+    //     }
+    // </style>
+    // <a href="https://team-hub.netlify.app/login">
+    // <button class="submit-button back-button">Back</button>
+    // </a>
+    // <p>Password is incorrect</p>
+    // `)
+    };
     const isValid = comparePassword(password, userDB.password)
     if (!isValid) {
-        res.status(401)
-        res.send(`
-        <style>
-            .submit-button {
-                font-family: Roboto;
-                font-size: 20px;
-                background-color: rgb(255, 255, 255);
-                height: 50px;
-                width: 100px;
-                border: none;
-                border-radius: 10px;
-                cursor: pointer;
-                transition: all 0.15s;
-            }
+        res.status(406)
+        res.json({"message": "Password is incorrect"})
+    //     res.send(`
+    //     <style>
+    //         .submit-button {
+    //             font-family: Roboto;
+    //             font-size: 20px;
+    //             background-color: rgb(255, 255, 255);
+    //             height: 50px;
+    //             width: 100px;
+    //             border: none;
+    //             border-radius: 10px;
+    //             cursor: pointer;
+    //             transition: all 0.15s;
+    //         }
             
-            .submit-button:hover {
-                color: white;
-                background-color: aqua;
-                transform: scale(1.2);
-            }
+    //         .submit-button:hover {
+    //             color: white;
+    //             background-color: aqua;
+    //             transform: scale(1.2);
+    //         }
         
 
-            .back-button {
-                height: 30px;
-                background-color: rgb(66,69,73);
-                color: white;
-            }
+    //         .back-button {
+    //             height: 30px;
+    //             background-color: rgb(66,69,73);
+    //             color: white;
+    //         }
             
-            .back-button:hover {
-                background-color: rgb(191,155,48);
-            }
-        </style>
-        <a href="https://team-hub.netlify.app/login">
-        <button class="submit-button back-button">Back</button>
-        </a>
-        <p>Password is incorrect</p>
-        `)
+    //         .back-button:hover {
+    //             background-color: rgb(191,155,48);
+    //         }
+    //     </style>
+    //     <a href="https://team-hub.netlify.app/login">
+    //     <button class="submit-button back-button">Back</button>
+    //     </a>
+    //     <p>Password is incorrect</p>
+    //     `)
 
     } else if (isValid) {
         req.session.user = userDB;
