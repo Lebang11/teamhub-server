1da70def8e3df0b8de9abe16b59d91dc6d681cf0
L.Nong <lnong023@student.wethinkcode.co.za>
2023-09-22 10:59:59 +0200

    trying to use database

--- public/Scripts/blogs.js
+++ public/Scripts/blogs.js
@@ -3,8 +3,13 @@ const { default: mongoose } = require('mongoose');
 const router = Router();
 const Blogs = require('../../database/Schema/Blogs');
 
-router.post('/', (req, res) => {
-    console.log(req.body)
+router.post('/', async (req, res) => {
+    const newBlog = await Blogs.create({author, title, text})
+    .then(user => {
+        console.log('done');
+        res.status(201);
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router;
