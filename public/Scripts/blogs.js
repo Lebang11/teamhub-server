0970af21a4f009c4e0e7658b9a6e8de2e9e7383d
L.Nong <lnong023@student.wethinkcode.co.za>
2023-09-22 12:00:49 +0200

    fix

--- public/Scripts/blogs.js
+++ public/Scripts/blogs.js
@@ -4,6 +4,11 @@ const router = Router();
 const Blogs = require('../../database/Schema/Blogs');
 
 router.post('/', async (req, res) => {
+    const author = req.body.author;
+    const title = req.body.title;
+    const text = req.body.text;
+
+
     const newBlog = await Blogs.create({author, title, text})
     .then(user => {
         console.log('done');
