b308b5e1753ccfb8ea42ff713cd5dd79e6e3dea4
lebang11 <lebangnong@gmail.com>
2023-09-22 22:02:59 +0200

    added access-control-allow-origin

--- public/Scripts/blogs.js
+++ public/Scripts/blogs.js
@@ -3,6 +3,12 @@ const { default: mongoose } = require('mongoose');
 const router = Router();
 const Blogs = require('../../database/Schema/Blogs');
 
+router.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+  });
+
 router.get('/', async (req, res) => {
     const data = await Blogs.find({})
     res.send(data)
