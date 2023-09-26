b0a05e0737bc252c9ce225c72f7d11f0617f4db2
L.Nong <lnong023@student.wethinkcode.co.za>
2023-09-26 15:32:59 +0200

    added date

--- public/Scripts/blogs.js
+++ public/Scripts/blogs.js
@@ -19,9 +19,10 @@ router.post('/', async (req, res) => {
     const author = req.body.author;
     const title = req.body.title;
     const description = req.body.text;
+    const date = new Date();
 
 
-    const newBlog = await Blogs.create({author, title, description})
+    const newBlog = await Blogs.create({author, title, description, date})
     console.log('Blog created by', author);
     res.status(201);
     res.json(newBlog);
