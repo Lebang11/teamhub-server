63ab3c0e52bac0d5163ddd15c0011acbe77464d1
lebang11 <lebangnong@gmail.com>
2023-09-29 23:35:42 +0200

    more testing

--- public/Scripts/problems.js
+++ public/Scripts/problems.js
@@ -4,19 +4,21 @@ const router = Router();
 const Problems = require('../../database/Schema/problems');
 const multer = require('multer');
 const Path = require('path');
+const Grid = require('gridfs-stream');
+Grid.mongo = mongo;
+let gfs = Grid(connection.db);
 
-const path = Path.resolve(__dirname, './var/data/1696016420591-prac.py')
+// set up connection to db for file storage
+const storage = require('multer-gridfs-storage')({
+   db: connection.db,
+   file: (req, file) => {
+      return {
+         filename: file.originalname
+      }
+   }
+});
 
-const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './var/data')
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname)
-    }
-})
-
-const upload = multer({ storage: fileStorage})
+const upload = multer({ storage: storage})
 
 router.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -30,18 +32,28 @@ router.get('/', async (req, res) => {
     res.download('./var/data/1696016420591-prac.py')
 })
 
-router.post('/', upload.single('file'), async (req, res) => {
-    const author = req.body.author;
-    const title = req.body.title;
-    const description = req.body.text;
-    const language = req.body.language;
-    const date = req.body.date;
-    const filename = Date.now() + '-' + req.body.filename;
+router.post('/', upload.single('file'), (req, res) => {
+    if (req.file) {
+       return res.json({
+          success: true,
+          file: req.file
+       });
+    }
+     res.send({ success: false });
+ });
 
-    const newProblem = await Problems.create({author, title, description, language, date, filename});
-    console.log('Problem created by', author);
-    res.status(201);
-    res.json(newProblem);
-})
+// router.post('/', upload.single('file'), async (req, res) => {
+//     const author = req.body.author;
+//     const title = req.body.title;
+//     const description = req.body.text;
+//     const language = req.body.language;
+//     const date = req.body.date;
+//     const filename = Date.now() + '-' + req.body.filename;
+
+//     const newProblem = await Problems.create({author, title, description, language, date, filename});
+//     console.log('Problem created by', author);
+//     res.status(201);
+//     res.json(newProblem);
+// })
 
 module.exports = router;
