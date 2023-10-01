c7bf6157b992089e780405ccbff6104939ad3c9a
lebang11 <lebangnong@gmail.com>
2023-10-01 22:49:32 +0200

    fix

--- public/Scripts/problems.js
+++ public/Scripts/problems.js
@@ -4,21 +4,7 @@ const router = Router();
 const Problems = require('../../database/Schema/problems');
 const multer = require('multer');
 const Path = require('path');
-const Grid = require('gridfs-stream');
-Grid.mongo = mongo;
-let gfs = Grid(connection.db);
 
-// set up connection to db for file storage
-const storage = require('multer-gridfs-storage')({
-   db: connection.db,
-   file: (req, file) => {
-      return {
-         filename: file.originalname
-      }
-   }
-});
-
-const upload = multer({ storage: storage})
 
 router.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -26,23 +12,8 @@ router.use((_req, res, next) => {
     next();
   });
 
-router.get('/', async (req, res) => {
-    // const data = await Problems.find({});
-    // res.send(data);
-    res.download('./var/data/1696016420591-prac.py')
-})
 
-router.post('/', upload.single('file'), (req, res) => {
-    if (req.file) {
-       return res.json({
-          success: true,
-          file: req.file
-       });
-    }
-     res.send({ success: false });
- });
-
-router.post('/', upload.single('file'), async (req, res) => {
+router.post('/', async (req, res) => {
     const author = req.body.author;
     const title = req.body.title;
     const description = req.body.text;
--- server.js
+++ server.js
@@ -58,14 +58,14 @@ app.use('/api/problems', problemsPage);
 
 
 
-const createURL = "https://team-hub.onrender.com/create"
-const mainURL = "https://team-hub.onrender.com"
-app.listen(PORT, () => console.log(`Now listening at ${mainURL}`));
+// const createURL = "https://team-hub.onrender.com/create"
+// const mainURL = "https://team-hub.onrender.com"
+// app.listen(PORT, () => console.log(`Now listening at ${mainURL}`));
 
 //Testing :
-// const createURL = `http://localhost:${PORT}/create`
-// const mainURL = `http://localhost:${PORT}`
-// app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
+const createURL = `http://localhost:${PORT}/create`
+const mainURL = `http://localhost:${PORT}`
+app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
 
 // app.use((req, res, next) => {
 //     if (req.session.user) next();
