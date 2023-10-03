37cbd6c2b6827fbf4d6a765d8cd2a5ef2e115271
L.Nong <lnong023@student.wethinkcode.co.za>
2023-10-03 14:26:48 +0200

    added image to user

--- database/Schema/User.js
+++ database/Schema/User.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 UserSchema = new mongoose.Schema({
     username: mongoose.SchemaTypes.String,
     email: mongoose.SchemaTypes.String,
-    password: mongoose.SchemaTypes.String
+    password: mongoose.SchemaTypes.String,
+    imagename: mongoose.SchemaTypes.String
 });
 
 module.exports = mongoose.model('users', UserSchema);
