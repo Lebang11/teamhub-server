7ed05f288c28f0b96a22b943e7c74a9445318c4e
L.Nong <lnong023@student.wethinkcode.co.za>
2023-10-12 10:32:08 +0200

    added app password

--- public/Scripts/email.js
+++ public/Scripts/email.js
@@ -37,7 +37,7 @@ router.post('/', async (req,res) => {
         service: 'gmail',
         auth: {
             user: 'lebang.teamhub@gmail.com',
-            pass: "LebangTeam"
+            pass: "hooa onbz wbii rwhz "
         }
     });
 
