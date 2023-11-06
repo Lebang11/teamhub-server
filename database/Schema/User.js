aaabce797146ce5e197c8ab86923598c28abe3b4
L.Nong <lnong023@student.wethinkcode.co.za>
2023-11-06 14:09:45 +0200

    added email and about to change

--- database/Schema/User.js
+++ database/Schema/User.js
@@ -4,7 +4,10 @@ UserSchema = new mongoose.Schema({
     username: mongoose.SchemaTypes.String,
     email: mongoose.SchemaTypes.String,
     password: mongoose.SchemaTypes.String,
-    imagename: mongoose.SchemaTypes.String
+    imagename: mongoose.SchemaTypes.String,
+    about: mongoose.SchemaTypes.String,
+    github: mongoose.SchemaTypes.String,
+    discord: mongoose.SchemaTypes.String
 });
 
 module.exports = mongoose.model('users', UserSchema);
